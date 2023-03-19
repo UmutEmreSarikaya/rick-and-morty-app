9b03813138fba6b98807b859bd319df32856b68c
Umut Sarıkaya <u.emresarikaya@gmail.com>
2023-03-19 17:25:12 +0300

    Splash screen added

--- app/src/main/java/com/umut/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/umut/rickandmortyapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.umut.rickandmortyapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.umut.rickandmortyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val isFirstTime = sharedPref.getBoolean(getString(R.string.first_time_boolean_key), true)
+
+        if(isFirstTime){
+            sharedPref.edit().putBoolean(getString(R.string.first_time_boolean_key), false).apply()
+            binding.welcomeText.text = getString(R.string.welcome_text)
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
     }
+
 }
